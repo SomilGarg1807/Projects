578dd47cc49e11360ae59263dfb6108efa5e214f
SomilGarg1807 <gargsomil9@gmail.com>
2024-09-15 19:26:22 +0530

    Nike Landing Page Project

---
+++ Nike Project/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Subscribee from "./Subscribee";
+
+export{
+    Hero,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    SuperQuality,
+    Subscribee,
+    CustomerReviews,
+    Footer
+}
